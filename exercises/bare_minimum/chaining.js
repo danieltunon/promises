49d20bf0723716233e51ed3ac8c5cd258546d31a
danieltunon <danieltunon@gmail.com>
2016-03-23 15:16:15 -0700

    Pass chaining tests

--- exercises/bare_minimum/chaining.js
+++ exercises/bare_minimum/chaining.js
@@ -79,14 +79,14 @@ var addNewUserToDatabaseAsync = function(user) {
 // Uncomment the lines below and run the example with `node exercises/bare_minimum/chaining.js`
 // It will succeed most of the time, but fail occasionally to demonstrate error handling
 
-// addNewUserToDatabaseAsync({ name: 'Dan', password: 'chickennuggets' })
-//   .then(function(savedUser) {
-//     console.log('All done!')
-//   })
-//   .catch(function(err) {
-//     // Will catch any promise rejections or thrown errors in the chain!
-//     console.log('Oops, caught an error: ', err.message)
-//   });
+addNewUserToDatabaseAsync({ name: 'Dan', password: 'chickennuggets' })
+  .then(function(savedUser) {
+    console.log('All done!')
+  })
+  .catch(function(err) {
+    // Will catch any promise rejections or thrown errors in the chain!
+    console.log('Oops, caught an error: ', err.message)
+  });
 
 /******************************************************************
  *                         Exercises                              *
@@ -103,11 +103,42 @@ var pluckFirstLineFromFileAsync = require('./promiseConstructor').pluckFirstLine
 var getGitHubProfileAsync = require('./promisification').getGitHubProfileAsync
 
 
+var writeFileAsync = Promise.promisify(fs.writeFile);
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+
+  return pluckFirstLineFromFileAsync(readFilePath).then(function(userName) {
+    return userName;
+  }).then(function(userName) {
+    return getGitHubProfileAsync(userName);
+  }).then(function(profile) {
+    return writeFileAsync(writeFilePath, JSON.stringify(profile), 'utf8');
+  })
+  
 };
 
 module.exports = {
   fetchProfileAndWriteToFile: fetchProfileAndWriteToFile
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
